3b565ad14bd9a9dab58dc318d4440155d048de42
kurtprof <adaltin@prof.educacao.sp.gov.br>
2024-07-23 23:10:18 -0300

    00.final

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-let nomeUsuario = prompt("Qual o seu nome?");
+let nomeUsuario = "";
 let elemento = document.querySelector("#nome-usuario");
-elemento.textContent = nomeUsuario;
+
+while(nomeUsuario == ""){
+    nomeUsuario = prompt("Qual o seu nome?")
+}
+
+if(nomeUsuario == null){
+    elemento.textContent = "Seja muito bem vindo";
+}else{
+    elemento.textContent = nomeUsuario;
+}
